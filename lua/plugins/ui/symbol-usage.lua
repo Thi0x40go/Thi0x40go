e0c59ac74155a8a5a17b25f9eacd7c341bb0d9cf
Thiago Pinheiro <thiagup@outlook.com>
2026-05-16 17:39:45 -0300

    fix(symbol-usage): fix git blame logic and improve reliability

--- lua/plugins/ui/symbol-usage.lua
+++ lua/plugins/ui/symbol-usage.lua
@@ -3,14 +3,29 @@ return {
     "Wansmer/symbol-usage.nvim",
     event = "LspAttach", -- Carrega assim que um servidor LSP (C#, Java, etc) se conectar
     opts = {
+      disable = { filetypes = { "css", "scss", "less" } },
       -- Configuração visual estilo IntelliJ
       text_format = function(symbol)
         local res = {}
-        if symbol.references and symbol.references > 0 then
+
+        -- 1. Contador de Usos (LSP)
+        if symbol and symbol.references and symbol.references > 0 then
           local usage = symbol.references <= 1 and "uso" or "usos"
           table.insert(res, string.format("󰌹 %s %s", symbol.references, usage))
         end
-        return table.concat(res, " ")
+
+        -- 2. Git Blame (Quem mexeu por último)
+        local ok, gitsigns = pcall(require, "gitsigns")
+        if ok and symbol and symbol.bufnr then
+          local line_data = vim.api.nvim_buf_get_var(symbol.bufnr, "gitsigns_blame_line_dict")
+          if line_data and line_data.author and line_data.author ~= "Not Committed Yet" then
+            local date = os.date("%d/%m/%y", line_data.author_time)
+            table.insert(res, string.format(" %s, %s", line_data.author, date))
+          end
+        end
+
+        if #res == 0 then return nil end
+        return "  " .. table.concat(res, " • ")
       end,
       -- Define em quais tipos de símbolos ele deve mostrar a contagem
       kinds = {
@@ -19,7 +34,8 @@ return {
         vim.lsp.protocol.SymbolKind.Function,
         vim.lsp.protocol.SymbolKind.Interface,
       },
-      vt_role = "above", -- Mostra acima da linha (estilo CodeLens)
+      vt_position = "end_of_line", -- Nome correto do parâmetro: vt_position
+      request_pending_text = false, -- Desativa o "loading..." chato
     },
   },
 }
